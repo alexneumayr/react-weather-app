1f0b3ff6f847c145fb103cde0e9ebdd4937529d6
alexneumayr <alex.nmr@outlook.com>
2025-01-25 14:01:20 +0100

    Add getLocation()

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
 import './App.css';
+import { useEffect, useState } from 'react';
 
 export default function App() {
-  return <></>;
+  const [location, setLocation] = useState();
+
+  useEffect(() => {
+    getLocation();
+  }, []);
+
+  function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setLocation({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          });
+          console.log(
+            `Latitude: ${position.coords.latitude}, Longitude: ${position.coords.longitude}`,
+          );
+        },
+        () => console.log('Unable to retrieve your location'),
+      );
+    } else {
+      console.log('Geolocation not supported');
+    }
+  }
+
+  return <div></div>;
 }
