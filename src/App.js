5097b8f8bfa357c6cd4ca86cef4bf388123d9a60
alexneumayr <alex.nmr@outlook.com>
2025-01-25 14:02:18 +0100

    Add API request

--- src/App.js
+++ src/App.js
@@ -3,10 +3,33 @@ import { useEffect, useState } from 'react';
 
 export default function App() {
   const [location, setLocation] = useState();
+  const [weatherData, setWeatherData] = useState();
 
   useEffect(() => {
     getLocation();
   }, []);
+
+  useEffect(() => {
+    if (location) {
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=${process.env.REACT_APP_API_KEY}&units=metric`,
+      )
+        .then(
+          (response) => {
+            if (response.ok) {
+              return response.json();
+            }
+            throw new Error('Request failed!');
+          },
+          (networkError) => console.log(networkError.message),
+        )
+        .then((jsonResponse) => {
+          setWeatherData(jsonResponse);
+          console.log(jsonResponse);
+        })
+        .catch((error) => console.log(error));
+    }
+  }, [location]);
 
   function getLocation() {
     if (navigator.geolocation) {
