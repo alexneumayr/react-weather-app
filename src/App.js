cae2d867fd6cc3c209ba205ef877f9eaef471bf9
alexneumayr <alex.nmr@outlook.com>
2025-01-27 14:29:40 +0100

    Implement full ReactRouter functionality

--- src/App.js
+++ src/App.js
@@ -6,68 +6,12 @@ import styles from './index.css';
 import ShowWeather from './ShowWeather';
 
 export default function App() {
-  const [location, setLocation] = useState();
-  const [weatherData, setWeatherData] = useState();
-  const [locationList, setLocationList] = useState([]);
-  const [cityNames, setCityNames] = useState([]);
   const [locationName, setLocationName] = useState('');
   const [locationPossibilities, setLocationPossibilities] = useState();
 
   useEffect(() => {
-    getLocation();
+    setLocationPossibilities([]);
   }, []);
-
-  useEffect(() => {
-    console.log(locationList);
-    if (locationList) {
-      setCityNames(
-        locationList.map((item) => {
-          return `${item.name}${item.state ? item.state : ''}, ${item.country}`;
-        }),
-      );
-    }
-  }, [locationList]);
-
-  useEffect(() => {
-    if (location) {
-      fetch(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=${process.env.REACT_APP_API_KEY}&units=metric`,
-      )
-        .then(
-          (response) => {
-            if (response.ok) {
-              return response.json();
-            }
-            throw new Error('Request failed!');
-          },
-          (networkError) => console.log(networkError.message),
-        )
-        .then((jsonResponse) => {
-          setWeatherData(jsonResponse);
-          console.log(jsonResponse);
-        })
-        .catch((error) => console.log(error));
-    }
-  }, [location]);
-
-  function getLocation() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          setLocation({
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude,
-          });
-          console.log(
-            `Latitude: ${position.coords.latitude}, Longitude: ${position.coords.longitude}`,
-          );
-        },
-        () => console.log('Unable to retrieve your location'),
-      );
-    } else {
-      console.log('Geolocation not supported');
-    }
-  }
 
   function HandleFormSubmit(event) {
     event.preventDefault();
@@ -93,28 +37,26 @@ export default function App() {
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/:lat/:lon" element={<ShowWeather />} />
+          <Route
+            path="/"
+            element={
+              <ShowWeather
+                setLocationPossibilities={setLocationPossibilities}
+                setLocationName={setLocationName}
+              />
+            }
+          />
+          <Route
+            path="/:lat/:lon"
+            element={
+              <ShowWeather
+                setLocationPossibilities={setLocationPossibilities}
+                setLocationName={setLocationName}
+              />
+            }
+          />
         </Routes>
-        {!location && <p>Please enable location services</p>}
-        {weatherData && (
-          <div>
-            <p>Location: {weatherData.name}</p>
-            Weather:
-            <ul>
-              {weatherData.weather.map((weatherCondition) => {
-                return (
-                  <li key={`weatherCondition-${weatherCondition.id}`}>
-                    {weatherCondition.main}: {weatherCondition.description}
-                  </li>
-                );
-              })}
-            </ul>
-            <p>Temperature: {weatherData.main.temp} &#176;C </p>
-            <p>Feels like: {weatherData.main.feels_like} &#176;C</p>
-            <p>Humidity: {weatherData.main.humidity}%</p>
-            <p>Wind: {Math.round(weatherData.wind.speed * 3.6)} km/h</p>
-          </div>
-        )}
+
         <div style={{ width: '300px' }}>
           <form onSubmit={HandleFormSubmit}>
             <label htmlFor="location-name">Location: </label>
@@ -133,7 +75,6 @@ export default function App() {
                     <li key={`location-${singleLocation.lat}`}>
                       <Link
                         to={`/${singleLocation.lat}/${singleLocation.lon}`}
-                        element={ShowWeather}
                       >{`${singleLocation.name}${singleLocation.state ? `, ${singleLocation.state}` : ''}, ${singleLocation.country}`}</Link>
                     </li>
                   );
--- src/ShowWeather.js
+++ src/ShowWeather.js
@@ -1,13 +1,90 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
-export default function ShowWeather() {
+export default function ShowWeather({
+  setLocationPossibilities,
+  setLocationName,
+}) {
   const params = useParams();
   const lat = params.lat;
   const lon = params.lon;
+  const [location, setLocation] = useState();
+  const [weatherData, setWeatherData] = useState();
+
+  useEffect(() => {
+    getLocation();
+  }, []);
+
+  useEffect(() => {
+    setLocationPossibilities([]);
+    setLocationName('');
+  }, [setLocationPossibilities, setLocationName, lat, lon]);
+
+  useEffect(() => {
+    if (location && !lat && !lon) {
+      fetchWeatherData(location.latitude, location.longitude);
+    } else if (lat && lon) {
+      fetchWeatherData(lat, lon);
+    }
+  }, [location, lon, lat]);
+
+  function fetchWeatherData(finalLatitude, finalLongitude) {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${finalLatitude}&lon=${finalLongitude}&appid=${process.env.REACT_APP_API_KEY}&units=metric`,
+    )
+      .then(
+        (response) => {
+          if (response.ok) {
+            return response.json();
+          }
+          throw new Error('Request failed!');
+        },
+        (networkError) => console.log(networkError.message),
+      )
+      .then((jsonResponse) => {
+        setWeatherData(jsonResponse);
+      })
+      .catch((error) => console.log(error));
+  }
+
+  function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setLocation({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          });
+        },
+        () => console.log('Unable to retrieve your location'),
+      );
+    } else {
+      console.log('Geolocation not supported');
+    }
+  }
+
   return (
     <>
-      Lat: {lat} <br />
-      Lon: {lon}
+      {!location && <p>Please enable location services</p>}
+      {weatherData && (
+        <div>
+          <p>Location: {weatherData.name}</p>
+          Weather:
+          <ul>
+            {weatherData.weather.map((weatherCondition) => {
+              return (
+                <li key={`weatherCondition-${weatherCondition.id}`}>
+                  {weatherCondition.main}: {weatherCondition.description}
+                </li>
+              );
+            })}
+          </ul>
+          <p>Temperature: {weatherData.main.temp} &#176;C </p>
+          <p>Feels like: {weatherData.main.feels_like} &#176;C</p>
+          <p>Humidity: {weatherData.main.humidity}%</p>
+          <p>Wind: {Math.round(weatherData.wind.speed * 3.6)} km/h</p>
+        </div>
+      )}
     </>
   );
 }
